043d2bc2015aef6344122cc20b7e490ffba0a542
Daniel Mutinda <daniel.mutinda@dataoptions.co.ke>
2018-02-26 23:40:28 +0300

    Refactored the navigation bar by adding contacts links

--- src/hcbc/views/layout/header.php
+++ src/hcbc/views/layout/header.php
@@ -76,6 +76,21 @@
 					<li><a href="#">Reports</a></li>
 				</ul>
 				<!-- authenticated menus -->
+
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> 
+							<i class="fa fa-phone"></i> +254 722 000 001 <i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#"><i class="fa fa-phone"></i> Queries: +254 733 000 001</a></li>
+							<li><a href="#"><i class="fa fa-phone"></i> Support: +254 722 000 002</a></li>
+						</ul>
+					</li>
+					<li><a href="#"><i class="fa fa-envelope"></i> Contacts</a></li>
+				</ul>
+
+
 			</div>
 		</div>
 	</nav>
